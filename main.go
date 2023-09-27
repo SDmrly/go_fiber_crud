b89878262b4d9b46c5b14e55ecb597795d9f34d1
Sinan <demiralay.080@gmail.com>
2023-09-27 16:57:39 +0300

    init fonksiyonu kullanılıp database   işlemleri oraya taşındı. Uygulama ilk çalıştığında önce init fonksiyonu içindeki database işlemlerini yapacak

--- main.go
+++ main.go
@@ -8,14 +8,23 @@ import (
 	"github.com/SDmrly/go_fiber_crud/utils"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 	"log"
 )
 
-func main() {
+var (
+	db  *gorm.DB
+	err error
+)
+
+func init() {
 	loadCnf, err := configs.LoadConfig(".")
 	utils.ErrorLogs("Could not load environment variables", err)
 
-	db := configs.DatabaseConnection(loadCnf)
+	db = configs.DatabaseConnection(loadCnf)
+}
+
+func main() {
 	validate := validator.New()
 
 	userRepo := apps.UserRepository(db)
